c04c0f7a07db620ec975eaf41197b90a1845df16
jesser1223 <jessereinink260@gmail.com>
2025-11-26 21:27:32 -0700

    Create database.py

---
+++ backend/database.py
@@ -0,0 +1,8 @@
+from sqlmodel import create_engine, SQLModel, Session
+
+DATABASE_URL = "sqlite:///./crm.db"
+engine = create_engine(DATABASE_URL, echo=False, connect_args={"check_same_thread": False})
+
+def get_session():
+    with Session(engine) as session:
+        yield session
